47e6a75c4ef9bdef4fe40d79c2743ba662e7ebf5
MikiJRs <emerttd@gmail.com>
2025-07-19 23:18:41 +0300

    axios configrated

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app: Application = express();
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   })
 );
